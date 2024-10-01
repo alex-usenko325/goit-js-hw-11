ae5cb2e62922a64a485f01de0fd5cd7a80dce648
Zergus325 <alex.usenko.97@gmail.com>
2024-10-01 13:33:39 +0300

    corect

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
-const loadMoreButton = document.getElementById('load-more');
 const loader = document.querySelector('.loader');
 let page = 1;
 let query = '';
@@ -20,14 +19,12 @@ form.addEventListener('submit', async event => {
 
   gallery.innerHTML = '';
   page = 1;
-  loadMoreButton.style.display = 'none';
   loader.style.display = 'block';
 
   try {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
     const data = await fetchImages(query, page);
     loader.style.display = 'none';
+
     if (data.hits.length === 0) {
       iziToast.error({
         message:
@@ -37,65 +34,15 @@ form.addEventListener('submit', async event => {
       });
       return;
     }
-    renderImages(data.hits);
-    lightbox.refresh();
-    loadMoreButton.style.display = 'block';
-  } catch (error) {
-    loader.style.display = 'none';
-    if (error.message === 'No images found') {
-      iziToast.error({
-        message:
-          'Sorry, no images found for your search query. Please try again!',
-        position: 'topRight',
-        class: 'custom-iziToast-error',
-      });
-    } else {
-      iziToast.error({
-        message: 'Something went wrong, please try again later.',
-        position: 'topRight',
-        class: 'custom-iziToast-error',
-      });
-    }
-  }
-});
-
-loadMoreButton.addEventListener('click', async () => {
-  page += 1;
-  loader.style.display = 'block';
-
-  try {
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
-    const data = await fetchImages(query, page);
-    loader.style.display = 'none';
-
-    if (!data.hits || data.hits.length === 0) {
-      iziToast.info({
-        message: 'No more images available.',
-        position: 'topRight',
-        class: 'custom-iziToast-info',
-      });
-      loadMoreButton.style.display = 'none';
-      return;
-    }
 
     renderImages(data.hits);
     lightbox.refresh();
   } catch (error) {
     loader.style.display = 'none';
-    if (error.message === 'No images found') {
-      iziToast.info({
-        message: 'No more images available.',
-        position: 'topRight',
-        class: 'custom-iziToast-info',
-      });
-      loadMoreButton.style.display = 'none';
-    } else {
-      iziToast.error({
-        message: 'Something went wrong, please try again later.',
-        position: 'topRight',
-        class: 'custom-iziToast-error',
-      });
-    }
+    iziToast.error({
+      message: 'Something went wrong, please try again later.',
+      position: 'topRight',
+      class: 'custom-iziToast-error',
+    });
   }
 });
